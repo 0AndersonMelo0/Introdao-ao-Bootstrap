4a312d02e21eda730b4a000915422d6993172776
0AndersonMelo0 <111611117+0AndersonMelo0@users.noreply.github.com>
2023-04-18 08:59:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const cep = document.getElementById('cep');
+
+cep.addEventListener('keyup', (e)=>{
+    let v = e.target.value.replace(/\D/, "");
+    v = v.replace(/(\d{5})(\d{3})/, "$1-$2");
+    e.target.value = v;
+});
+
+const button = document.getElementById('button');
+
+button.addEventListener('click',()=>{
+    const nome = document.getElementById('cep');
+    const data = document.getElementById('data');
+    const curso = document.getElementById('curso');
+    const turma = document.getElementById('turma');
+    const endereço = document.getElementById('endereço');
+    const nome_mae = document.getElementById('nome_mae');
+    const bairro = document.getElementById('bairro');
+    const nome_pai = document.getElementById('nome_pai');
+    const cidade = document.getElementById('cidade');
+
+    if(nome.value == '' | data.value == '' | curso .value == 'Selecione o Curso:' | turma.value == '' | endereço.value == '' | nome_mae.value == '' | nome_pai.value == '' | bairro.value == '' | cidade.value == ''){
+        alert("Há campos vazios!")
+    }else{
+        alert("Tudo ok!")
+    }
+});
